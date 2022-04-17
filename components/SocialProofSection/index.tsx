84b4a471165d2fcd996b627850ee4ea161b444eb
HanSeong Lee <jkhanseong@gmail.com>
2022-04-17 18:46:31 +0900

    ADD | SocialProofSection on desktop

--- components/SocialProofSection/index.tsx
+++ components/SocialProofSection/index.tsx
@@ -8,15 +8,18 @@ const SocialProofSection: React.FC = () => {
     return (
         <section className={styles.socialProofSection}>
             <Container className={styles.container}>
-                <h2 className={styles.title}>
-                    10,000+ of our users love our products.
-                </h2>
-                <p className={styles.description}>
-                    We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.
-                </p>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>
+                        10,000+ of our users love our products.
+                    </h2>
+                    <p className={styles.description}>
+                        We only provide great products combined with excellent customer service. See what our satisfied
+                        customers are saying about our services.
+                    </p>
+                </div>
 
-                <RateCardContainer className={styles.rateCardContainer} />
-                <TestimonialCardContainer className={styles.testimonialCardContainer} />
+                <RateCardContainer className={styles.rateCardContainer}/>
+                <TestimonialCardContainer className={styles.testimonialCardContainer}/>
             </Container>
         </section>
     );
